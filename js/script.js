6c0255631bd1297f8ff0020b43e5515da8ac137d
rofiqahmad22 <rofiqahmadm@gmail.com>
2023-08-09 09:32:39 +0700

    feat: add search form and search box

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// togle class active
+// togle class active maburger menu
 const navbarNav = document.querySelector(".navbar-nav");
 
 // menu onclick
@@ -6,10 +6,33 @@ document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
-// click outside menu
+// togle class active search form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+document.querySelector("#search-button").onclick = (event) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  event.preventDefault();
+};
+
+// click outside navbar
 const hamburger = document.querySelector("#hamburger-menu");
+
 document.addEventListener("click", function (event) {
   if (!hamburger.contains(event.target) && !navbarNav.contains(event.target)) {
     navbarNav.classList.remove("active");
+  }
+});
+
+// click outside search form
+const searchButton = document.querySelector("#search-button");
+
+document.addEventListener("click", function (event) {
+  if (
+    !searchButton.contains(event.target) &&
+    !searchForm.contains(event.target)
+  ) {
+    searchForm.classList.remove("active");
   }
 });
